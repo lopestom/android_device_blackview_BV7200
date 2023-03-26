49adc94c1ad703e7c383e42a87a11155fe00cb86
lopestorm <guillopes2002@gmail.com>
2023-03-26 18:02:38 -0300

    Update twrp_BV7200.mk

--- twrp_BV7200.mk
+++ twrp_BV7200.mk
@@ -27,6 +27,4 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31 \
-    ro.bootimage.build.date.utc=0 \
-    ro.build.date.utc=0
+    ro.vendor.build.security_patch=2099-12-31
